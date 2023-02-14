66a0253c27f95a6c84bfd74daebcb5e709f63dcf
Manuel <30572287+manuel-rw@users.noreply.github.com>
2023-02-14 21:56:28 +0100

    🔖 Tag version

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,7 +3,7 @@ export const Constants = {
     Version: {
       Major: 0,
       Minor: 0,
-      Patch: 3,
+      Patch: 4,
       All: () =>
         `${Constants.Metadata.Version.Major}.${Constants.Metadata.Version.Minor}.${Constants.Metadata.Version.Patch}`,
     },
