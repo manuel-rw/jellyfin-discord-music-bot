95dbb76f9f6aec9dc26f57660593e602222e4d5c
Manuel <30572287+manuel-rw@users.noreply.github.com>
2025-11-21 11:31:25 +0100

    chore: increment version

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,7 +2,7 @@ export const Constants = {
   Metadata: {
     Version: {
       Major: 1,
-      Minor: 0,
+      Minor: 1,
       Patch: 0,
       All: () =>
         `${Constants.Metadata.Version.Major}.${Constants.Metadata.Version.Minor}.${Constants.Metadata.Version.Patch}`,
